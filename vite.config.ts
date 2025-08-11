e3a810c5df28483a2281e07faef278c7b223f3df
Mateus Von <mateusvon205@gmail.com>
2025-08-11 01:19:21 -0300

    Package config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/four-years-and-counting/",
   server: {
     host: '::',
     port: 8080,
